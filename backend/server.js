d72a1ca0bb3905174199271d49bc9e015c438eff
Hanni <ngynngchn@gmail.com>
2023-04-18 22:43:33 +0200

    Added functions and endpoints to get boat and reservation infos

--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,46 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import multer from "multer";
-import { addBoat, addReservation } from "./utils/helper.js";
+import {
+	addBoat,
+	addReservation,
+	deleteBoat,
+	getBoat,
+	getBoatCount,
+	getBoats,
+	getReservation,
+	getSingleBooking,
+} from "./utils/helper.js";
 
 const PORT = process.env.PORT;
+const origin = process.env.VITE_FRONTEND;
 
+console.log(origin);
 const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
-server.use(cors(process.env.VITE_FRONTEND));
+server.use(cors());
 
 const upload = multer({ dest: "uploads/" });
 server.use("/uploads", express.static("./uploads"));
 
 // boats
-server.post("/api/v1/boats", upload.none(), addBoat);
-// server.post("/api/v1/boats", upload.single("pic"), addBoat);
+// boat form field endpoint
+server.post("/api/v1/boats", upload.single("pic"), addBoat);
+// get all boats endpoint
+server.get("/api/v1/boats", getBoats);
 
+server.get("/api/v1/boats-total", getBoatCount);
+
+server.get("/api/v1/boats/:id", getBoat);
+
+server.delete("/api/v1/boats/:id", deleteBoat);
 // reservations
+
 server.post("/api/v1/reservations", addReservation);
+
+server.get("/api/v1/reservations", getReservation);
+server.get("/api/v1/reservations/:id", getSingleBooking);
 
 server.listen(PORT, () => console.log("I am listening to PORT", PORT));
--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -1,16 +1,135 @@
+import { ObjectId } from "mongodb";
 import { getDB } from "./db.js";
 
 const COL = "boats";
 
 export const addBoat = async (req, res) => {
-	// console.log(req.file);
-	console.log(req.body);
-	// try {
-	// 	const db = await getDB();
-	// 	const response = await db.collection(COL).insertOne();
-	// } catch (err) {
-	// 	res.status(500).end();
-	// }
-	res.end;
+	try {
+		req.body.pic = req.file.path;
+		req.body.status = "FREE";
+		const db = await getDB();
+		const length = await db.collection(COL).countDocuments();
+		req.body.boatID = length + 1;
+		console.log(req.body);
+		const result = await db.collection(COL).insertOne(req.body);
+		res.send("Added Boat");
+	} catch (err) {
+		console.error(err.message); // log the error message
+		res.status(500).end();
+	}
+	// res.end;
 };
-export const addReservation = async (req, res) => {};
+
+export const getBoats = async (req, res) => {
+	try {
+		const db = await getDB();
+		const result = await db.collection(COL).find().toArray();
+		// console.log(result);
+		if (result === null) res.end();
+		else {
+			res.json(result);
+		}
+	} catch (err) {
+		console.error(err.message); // log the error message
+		res.status(500).end();
+	}
+};
+export const getBoatCount = async (req, res) => {
+	try {
+		const db = await getDB();
+		const length = await db.collection(COL).countDocuments();
+		console.log(length);
+		if (length === null) res.end();
+		else {
+			res.json(length);
+		}
+	} catch (err) {
+		console.error(err.message); // log the error message
+		res.status(500).end();
+	}
+};
+
+export const getBoat = async (req, res) => {
+	console.log(req.params.id);
+	try {
+		const db = await getDB();
+		const result = await db
+			.collection(COL)
+			.findOne({ _id: new ObjectId(req.params.id) });
+		if (result === null) res.end();
+		else {
+			console.log(result);
+			res.json(result);
+		}
+	} catch (err) {
+		console.error(err.message);
+		res.send(500).end();
+	}
+};
+
+export const deleteBoat = async (req, res) => {
+	try {
+		const db = await getDB();
+		const response = await db
+			.collection(COL)
+			.deleteOne({ _id: new ObjectId(req.params.id) });
+		if (response === null) res.end();
+		else {
+			res.json(response);
+		}
+	} catch (err) {
+		console.error(err.message);
+		res.send(500).end();
+	}
+};
+
+export const addReservation = async (req, res) => {
+	console.log(req.body);
+	// res.end(/);
+	try {
+		const db = await getDB();
+		const result = await db.collection("bookings").insertOne(req.body);
+		console.log(result);
+		if (result === null) res.end();
+		else {
+			res.json(result);
+		}
+	} catch (err) {
+		console.error(err.message);
+		res.send(500).end();
+	}
+};
+
+export const getReservation = async (req, res) => {
+	console.log(req.body);
+	try {
+		const db = await getDB();
+		const result = await db.collection("bookings").find().toArray();
+		// console.log(result);
+		if (result === null) res.end();
+		else {
+			res.json(result);
+		}
+	} catch (err) {
+		console.error(err.message); // log the error message
+		res.status(500).end();
+	}
+};
+
+export const getSingleBooking = async (req, res) => {
+	console.log(req.params.id);
+	try {
+		const db = await getDB();
+		const result = await db
+			.collection("bookings")
+			.findOne({ _id: new ObjectId(req.params.id) });
+		if (result === null) res.end();
+		else {
+			console.log(result);
+			res.json(result);
+		}
+	} catch (err) {
+		console.error(err.message);
+		res.send(500).end();
+	}
+};

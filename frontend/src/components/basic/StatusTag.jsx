fc48fa4e39d96dec3e9c5dd2598fb985a295d456
Hanni <ngynngchn@gmail.com>
2023-04-19 15:20:50 +0200

    Fixed status tag

--- frontend/src/components/basic/StatusTag.jsx
+++ frontend/src/components/basic/StatusTag.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 function StatusTag({ content }) {
+	console.log(content == "FREE");
 	return (
 		<div
 			className={
-				content == "FREE" || "PAID" ? "free status" : "reserved status"
+				content == "FREE" || content == "PAID"
+					? "free status"
+					: "reserved status"
 			}>
 			{content}
 		</div>
